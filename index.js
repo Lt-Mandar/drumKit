08d443d9f73d1a457a222672b846dd62479feb11
Lt-Mandar <mandarpowale@hotmail.com>
2021-10-12 23:22:43 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,76 @@
+// function to handle mouse click on buttons
+
+function handleClick() {
+  makeSound(this.innerHTML);
+  buttonAnimation(this.innerHTML);
+}
+
+//function to handle keyboard inputs
+
+function handleKey(event) {
+  makeSound(event.key);
+  buttonAnimation(event.key);
+}
+
+//function to generate sound
+
+function makeSound(key) {
+  switch (key) {
+    case "w":
+      var crash = new Audio('sounds/crash.mp3');
+      crash.play();
+    break;
+
+    case "a":
+      var kick = new Audio('sounds/kick-bass.mp3');
+      kick.play();
+    break;
+
+    case "s":
+      var snare = new Audio('sounds/snare.mp3');
+      snare.play();
+    break;
+
+    case "d":
+      var tom1 = new Audio('sounds/tom-1.mp3');
+      tom1.play();
+    break;
+
+    case "j":
+      var tom2 = new Audio('sounds/tom-2.mp3');
+      tom2.play();
+    break;
+
+    case "k":
+      var tom3 = new Audio('sounds/tom-3.mp3');
+      tom3.play();
+    break;
+
+    case "l":
+      var tom4 = new Audio('sounds/tom-4.mp3');
+      tom4.play();
+    break;
+
+    default: console.log(this.innerHTML);
+
+  }
+}
+
+//creating button animation function
+
+function buttonAnimation(currentKey) {
+  var activeButton = document.querySelector("." + currentKey);
+  activeButton.classList.add("pressed");
+  // make button animation fade
+  setTimeout(function() { activeButton.classList.remove("pressed"); }, 100);
+}
+
+//detecting mouse cliks
+
+for (i = 0; i < document.querySelectorAll(".drum").length; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", handleClick);
+}
+
+//detecting keyboard press
+
+document.addEventListener("keydown", handleKey);
